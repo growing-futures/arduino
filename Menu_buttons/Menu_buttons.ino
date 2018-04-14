61090c0ec7bd41e664694bb301f37350c0fff9d6
Darmator <eduardo.gonzalezgarcia01@gmail.com>
2018-04-14 19:37:13 -0400

    Added 1 more button

--- Menu_buttons/Menu_buttons.ino
+++ Menu_buttons/Menu_buttons.ino
@@ -1,24 +1,27 @@
+#define back 5
 #define enter 4
 #define next 3
 #define last 2
-bool nextState = LOW;
-bool lastState = LOW;
-bool enterState = false;
-bool pressedOnce = false;
+bool nextState = HIGH;
+bool lastState = HIGH;
+bool enterState = HIGH;
+bool backState = HIGH;
+bool pressedOnce = HIGH;
 unsigned long buttonOffset = 0;
 int buttonDelay = 300;
 void setup() {
   pinMode(enter,INPUT_PULLUP);
   pinMode(next, INPUT_PULLUP);
   pinMode(last, INPUT_PULLUP);
+  pinMode(back, INPUT_PULLUP);
   Serial.begin(9600);
-
 }
 
 void loop() {
   enterState = digitalRead(enter);
   nextState = digitalRead(next);
   lastState = digitalRead(last);
+  backState = digitalRead(back);
   if (millis() - buttonOffset > buttonDelay){
    if((nextState == LOW) && (!pressedOnce)){
     Serial.println("Next State");
@@ -32,6 +35,11 @@ void loop() {
   }
   else if((enterState == LOW) && (!pressedOnce)){
     Serial.println("Enter State");
+    pressedOnce = true;
+    buttonOffset = millis();
+  }
+  else if((backState == LOW) && (!pressedOnce)){
+    Serial.println("Back State");
     pressedOnce = true;
     buttonOffset = millis();
   }
